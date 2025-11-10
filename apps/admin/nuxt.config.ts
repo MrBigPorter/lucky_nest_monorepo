a220a34bf724ad0946e0433c17d9d53c97afb569
MrBigPorter <mrsuperporter@gmail.com>
2025-11-11 00:42:23 +0800

    feat(docker): restructure Docker setup and add base image for monorepo

--- apps/admin/nuxt.config.ts
+++ apps/admin/nuxt.config.ts
@@ -3,6 +3,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
     compatibilityDate: '2025-11-08',
+    runtimeConfig:{
+        apiBase: process.env.NUXT_API_BASE || 'http://localhost:3000',
+        public: {
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3000',
+        },
+    },
     devtools: { enabled: true },
 
     modules: ['@nuxt/ui'],
