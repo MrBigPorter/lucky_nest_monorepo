e9c60eb28b6e9750aa436f5d49c14e0404842159
MrBigPorter <mrsuperporter@gmail.com>
2025-11-20 22:36:26 +0800

    feat(wallet): add response DTOs for wallet balance, credit, and debit operations

---
+++ apps/api/src/wallet/dto/wallet-credit.response.dto.ts
@@ -0,0 +1,8 @@
+import {ApiProperty} from "@nestjs/swagger";
+
+export class WalletCreditResponseDto {
+   @ApiProperty({ description: 'Transaction Number', example: 'TXN1234567890', type: String})
+   transactionNo!: string;
+    @ApiProperty({ description: 'Updated Real Balance', example: '100.00', type: String})
+   realBalance!: string;
+}
