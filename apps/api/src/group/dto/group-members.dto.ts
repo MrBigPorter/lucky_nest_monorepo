a4d726dd26f24143bffe4a4b483b4bb097c776c6
MrBigPorter <mrsuperporter@gmail.com>
2025-11-24 23:20:12 +0800

    feat(group): add endpoint and DTOs for listing group members with pagination

---
+++ apps/api/src/group/dto/group-members.dto.ts
@@ -0,0 +1,21 @@
+import {ApiProperty} from "@nestjs/swagger";
+import {ToNumber} from "@api/common/dto/transforms";
+import {IsInt, IsString, Max, Min} from "class-validator";
+
+export class GroupMembersDto {
+    @ApiProperty({ description: 'groupId', example: '1', type: String})
+    @IsString()
+    groupId!: string;
+
+    @ApiProperty({ description: 'page', example: 1, type: Number})
+    @ToNumber()
+    @IsInt()
+    @Min(1)
+    page: number = 1;
+
+    @ApiProperty({ description: 'pageSize', example: 50, type: Number})
+    @ToNumber()
+    @Min(1)
+    @Max(100)
+    pageSize: number = 50;
+}
