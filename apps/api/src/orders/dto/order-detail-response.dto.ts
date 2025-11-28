68805ed05b489c5ed0a3cc4e037cc08598fa7e50
MrBigPorter <mrsuperporter@gmail.com>
2025-11-28 11:26:01 +0800

    feat(order): enhance order DTOs with additional fields and improve response structures

--- apps/api/src/orders/dto/order-detail-response.dto.ts
+++ apps/api/src/orders/dto/order-detail-response.dto.ts
@@ -1,5 +1,10 @@
-import {OrderItemDto} from "@api/orders/dto/order-item.dto";
+import {OrderItemDto, WalletTransactionDto} from "@api/orders/dto/order-item.dto";
+import { ApiProperty } from "@nestjs/swagger";
 
+/**
+ * Order detail response DTO extending OrderItemDto and including wallet transactions.
+ */
 export class OrderDetailResponseDto extends OrderItemDto{
-
+    @ApiProperty({ description: 'wallet transactions', type: [WalletTransactionDto]})
+    transactions!: WalletTransactionDto[];
 }
