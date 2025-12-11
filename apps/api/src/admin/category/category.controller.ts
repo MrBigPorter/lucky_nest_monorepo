a6424b1a882b7371923231b6e43bfa56f2845c35
MrBigPorter <mrsuperporter@gmail.com>
2025-12-11 21:09:40 +0800

    feat(order): add Order module with CRUD operations and pagination for order management

--- apps/api/src/admin/category/category.controller.ts
+++ apps/api/src/admin/category/category.controller.ts
@@ -18,31 +18,48 @@ import { Role } from '@lucky/shared';
 import { CreateCategoryDto } from '@api/admin/category/dto/create-category.dto';
 import { UpdateCategoryDto } from '@api/admin/category/dto/update-category.dto';
 
-@ApiTags('后台-分类管理')
+@ApiTags('Admin Category Management')
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('admin/category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  /**
+   * Create a new category
+   * @param dto
+   */
   @Post('create')
   @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   async create(@Body() dto: CreateCategoryDto) {
     return this.categoryService.create(dto);
   }
 
+  /**
+   * Get category list
+   *
+   */
   @Get('list')
   @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.EDITOR, Role.VIEWER)
   async findAll() {
     return this.categoryService.findAll();
   }
 
+  /**
+   * Get category details by ID
+   * @param id
+   */
   @Get(':id')
   @Roles(Role.ADMIN, Role.SUPER_ADMIN, Role.EDITOR, Role.VIEWER)
   async findOne(@Param('id', ParseIntPipe) id: number) {
     return this.categoryService.findOne(id);
   }
 
+  /**
+   * Update an existing category
+   * @param id
+   * @param dto
+   */
   @Patch(':id')
   @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   async update(
@@ -52,6 +69,11 @@ export class CategoryController {
     return this.categoryService.update(id, dto);
   }
 
+  /**
+   * Update category state
+   * @param id
+   * @param state
+   */
   @Patch(':id/state')
   @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   async updateState(
@@ -61,6 +83,10 @@ export class CategoryController {
     return this.categoryService.updateState(id, state);
   }
 
+  /**
+   * Delete a category by ID
+   * @param id
+   */
   @Delete(':id')
   @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   async remove(@Param('id', ParseIntPipe) id: number) {
