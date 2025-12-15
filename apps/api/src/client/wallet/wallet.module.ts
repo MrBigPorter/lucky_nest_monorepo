52a48a1db201f626e5c4fba3d6f75c6021ed48c5
MrBigPorter <mrsuperporter@gmail.com>
2025-12-16 00:18:55 +0800

    feat(dependencies): refactor circular dependency handling between Wallet and Payment modules

--- apps/api/src/client/wallet/wallet.module.ts
+++ apps/api/src/client/wallet/wallet.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { WalletService } from '@api/client/wallet/wallet.service';
 import { WalletController } from '@api/client/wallet/wallet.controller';
 import { ClientWalletService } from '@api/client/wallet/client-wallet.service';
+import { PaymentModule } from '@api/client/payment/payment.module';
 
 @Module({
+  imports: [forwardRef(() => PaymentModule)],
   providers: [WalletService, ClientWalletService],
-  exports: [WalletService],
+  exports: [WalletService, ClientWalletService],
   controllers: [WalletController],
 })
 export class WalletModule {}
