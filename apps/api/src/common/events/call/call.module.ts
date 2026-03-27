d82c4795434f4915f7ab69cd40b3c0b6080aed05
MrBigPorter <mrsuperporter@gmail.com>
2026-03-27 15:50:46 +0800

    feat: implement call end message handling with chat integration and enhanced payload structure

--- apps/api/src/common/events/call/call.module.ts
+++ apps/api/src/common/events/call/call.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CallGateway } from './call.gateway';
+import { ChatModule } from '@api/common/chat/chat.module';
 
 @Module({
+  imports: [ChatModule],
   providers: [CallGateway],
 })
 export class CallModule {}
